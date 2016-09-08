92fa3c16455b8197ca7834432ac9f4f3fb3826e8
alexberry721 <alexberry721@gmail.com>
2016-09-08 06:06:32 -0700

    Debugged

--- Debugging Exercise/main.cpp
+++ Debugging Exercise/main.cpp
@@ -5,11 +5,50 @@
 //  Created by ax on 9/8/16.
 //  Copyright © 2016 COMP130. All rights reserved.
 //
+//  Book exercise 3.7 - Sort three integers
+//
+//  TODO: accommodate equivalent inputs in ranking, e.g. input 4 8 4
 
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    cout << "Sort Three Integers \n";
+    // D
+    int a = 0.0, b = 0.0, c = 0.0;
+    
+    // I
+    cout << "Enter three integers (1 2 3): ";
+    cin >> a >> b >> c;
+    
+    // P // O
+    
+    if (a < b && a < c && b < c)
+    {
+        cout << a << ' ' << b << ' ' << c << endl;
+    }
+    else if (a < b && a < c && b > c)
+    {
+        cout << a << ' ' << c << ' ' << b << endl;
+    }
+    else if (a < b && a > c)
+    {
+        cout << c << ' ' << a << ' ' << b << endl;
+    }
+    else if (a > b && a < c)
+    {
+        cout << b << ' ' << a << ' ' << c << endl;
+    }
+    else if (a > b && a > c && b < c)
+    {
+        cout << b << ' ' << c << ' ' << a << endl;
+    }
+    else if (a > b && a > c && b > c)
+    {
+        cout << c << ' ' << b << ' ' << a << endl;
+    }
+    
     return 0;
 }
